1649201f23d97b346d2e9053a58ed1dbad653547
uttamsutariya <uttam.sutariya@capitsign.com>
2024-01-10 14:35:59 +0530

    evently setup done

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+    return (
+        <main>
+            <Button className="px-10">Click me</Button>
+        </main>
+    );
+}
