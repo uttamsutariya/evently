7e82529f7904a7aff337f4f6dc4b4ad62dd47836
uttamsutariya <uttam.sutariya@capitsign.com>
2024-01-10 19:10:37 +0530

    done with header

---
+++ components/shared/MobileNav.tsx
@@ -0,0 +1,23 @@
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import Image from "next/image";
+import { Separator } from "../ui/separator";
+import NavItems from "./NavItems";
+
+const MobileNav = () => {
+    return (
+        <nav className="md:hidden">
+            <Sheet>
+                <SheetTrigger className="align-middle">
+                    <Image src="/assets/icons/menu.svg" alt="menu icon" width={24} height={24} className="cursor-pointer" />
+                </SheetTrigger>
+                <SheetContent className="flex flex-col md:hidden bg-white gap-6">
+                    <Image src="/assets/images/logo.svg" alt="Logo" width={128} height={38} />
+                    <Separator className="border border-gray-200" />
+                    <NavItems />
+                </SheetContent>
+            </Sheet>
+        </nav>
+    );
+};
+
+export default MobileNav;
