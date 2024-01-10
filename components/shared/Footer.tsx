176c4f5df4617d370ee0ae6989c4a7ddbba4abb9
uttamsutariya <uttam.sutariya@capitsign.com>
2024-01-10 19:25:22 +0530

    done with footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,5 +1,17 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <footer className="border-t">
+            <div className="flex-center wrapper flex-between flex flex-col sm:flex-row gap-4 p-5 text-center">
+                <Link href={"/"}>
+                    <Image src={"/assets/images/logo.svg"} width={128} height={38} alt="logo" />
+                </Link>
+                <p>2024 Evently. All Rights Reserved.</p>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
