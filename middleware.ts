d7b73a152fd5a05d7eea3695344462330d995ac7
uttamsutariya <uttam.sutariya@capitsign.com>
2024-01-11 10:37:54 +0530

    fixed middleware for public routes

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,8 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-    publicRoutes: ["/", "/events/:id", "/api/webhook/clerk", "/api/webhook/stripe", "/api/uploadthing"],
-    ignoredRoutes: ["/api/webhook/clerk", "/api/webhook/stripe", "/api/uploadthing"],
+    publicRoutes: ["/", "/events/:id", "/api/webhooks/clerk", "/api/webhooks/stripe", "/api/uploadthing"],
+    ignoredRoutes: ["/api/webhooks/clerk", "/api/webhooks/stripe", "/api/uploadthing"],
 });
 
 export const config = {
